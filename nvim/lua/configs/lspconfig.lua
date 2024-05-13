7ddcfbfbaf398cb03a6988aba57faed9511f4241
Ayush Srivastava <ayush.srivastava@vimaan.ai>
2024-05-13 20:38:33 +0530

    first commit

---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,31 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "ansiblels", "docker_compose_language_service", "bashls" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+lspconfig.ansiblels.setup{
+  filetypes = { "yaml" }
+}
+
+lspconfig.gopls.setup{}
+lspconfig.golangci_lint_ls.setup{}
+lspconfig.dockerls.setup{}
---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,32 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('i','kkk','<cmd>lua vim.lsp.buf.completion()<CR>')
+map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+map('n','<leader>bn','<cmd>bnext<CR>')
+map('n','<leader>bp','<cmd>bprevious<CR>')
+map('n','<leader>nj','ddpj<CR>')
+map('n','<leader>nk','ddkkpk<CR>')
+map('n','<leader>lss', '<cmd>LspStop<CR>')
+map('n','<leader>lst', '<cmd>LspStart<CR>')
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
